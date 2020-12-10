fb24e9f165d6d9a5b9698587be97cbe5df658e77
arilas-rouaiguia <71872334+arilas-rouaiguia@users.noreply.github.com>
2020-12-10 15:58:37 +0100

    Add files via upload

---
+++ lpdo.php
@@ -0,0 +1,15 @@
+<?php
+
+class lpdo
+{
+
+function __construct ($host, $username, $password, $db){}
+function __connect (){}
+function __destroy (){}
+function __close (){}
+function __execute ($query){}
+function __getLastQuery(){}
+function __getLastResult(){}
+function __getTables(){}
+function __getFields($table){}
+}
---
+++ user.php
@@ -0,0 +1,24 @@
+<?php
+
+class user
+{
+	private $id;
+	public $login;
+	public $email;
+	public $firstname;
+	public $lastname;
+
+public function register($login, $password, $email, $firstname,
+$lastname){}
+public function connect($login, $password){}
+public function disconnect(){}
+public function delete(){}
+public function update(){}
+public function isConnected(){}
+public function getAllInfos(){}
+public function getLogin(){}
+public function getEmail(){}
+public function getFirstname(){}
+public function getLastname(){}
+public function refresh(){}
+}
